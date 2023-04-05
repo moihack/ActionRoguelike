04a3cc38e27f26a9d481baf1bc653596953dc486
moihack <moihack@users.noreply.github.com>
2023-04-05 18:20:18 +0300

    L11 : Custom C++ BT Task for Ranged Attacks Created custom BT Task in C++ and used that in MinionRangedBT. Updated MinionRangedBT to allow the AI enemy to attack (spawn a projectile) the player every 2 seconds and also constantly rotate the AI Pawn to where the player is (service Set default focus).

---
+++ Source/ActionRoguelike/Public/AI/SBTTask_RangedAttack.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "SBTTask_RangedAttack.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONROGUELIKE_API USBTTask_RangedAttack : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+	// copy-pasted from header #include "BehaviorTree/BTTaskNode.h"
+	/** starts this task, should return Succeeded, Failed or InProgress
+	 *  (use FinishLatentTask() when returning InProgress)
+	 * this function should be considered as const (don't modify state of object) if node is not instanced! */
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+protected:
+	UPROPERTY(EditAnywhere, Category = "AI")
+	TSubclassOf<AActor> ProjectileClass;
+
+};
