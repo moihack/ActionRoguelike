cca526d1d89e32edc5f0409cbefcf45511870c0d
moihack <moihack@users.noreply.github.com>
2023-04-07 00:20:57 +0300

    L11 : Adding "Sight" with Pawn Sensing Component
    Commented out some lines in SAIController. SAICharacter will replace that functionality instead.
    Updated SAICharacter to include a PawnSensingComponent and binded function OnPawnSeen to OnSeePawn delegate. SAICharacter::OnPawnSeen now sets the TargetActor key in Blackboard (instead of SAIController).
    Updated MinionRangedBP's PawnSensingComponent PeripheralVisionAngle and lowered Pawn's BaseEyeHeight.
    Updated QueryContext_TargetActor to solve an issue where the AI pawn would now get stuck if not having seen the player at start of game.

--- Source/ActionRoguelike/Private/AI/SAIController.cpp
+++ Source/ActionRoguelike/Private/AI/SAIController.cpp
@@ -11,12 +11,13 @@ void ASAIController::BeginPlay()
 
 	RunBehaviorTree(BehaviorTree);
 
-	APawn* MyPawn = UGameplayStatics::GetPlayerPawn(this, 0);
-	if (MyPawn)
-	{
-		GetBlackboardComponent()->SetValueAsVector("MoveToLocation", MyPawn->GetActorLocation());
+	// below lines were commented out since ASAICharacter::OnPawnSeen is now responsible for setting TargetActor value.
+	//APawn* MyPawn = UGameplayStatics::GetPlayerPawn(this, 0);
+	//if (MyPawn)
+	//{
+	//	GetBlackboardComponent()->SetValueAsVector("MoveToLocation", MyPawn->GetActorLocation());
 
-		GetBlackboardComponent()->SetValueAsObject("TargetActor", MyPawn);
-	}
+	//	GetBlackboardComponent()->SetValueAsObject("TargetActor", MyPawn);
+	//}
 }
 
