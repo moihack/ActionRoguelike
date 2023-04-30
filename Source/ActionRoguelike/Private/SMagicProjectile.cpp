aa1f5f4c6fc8e4f201f16f941671f597669ebb37
moihack <moihack@users.noreply.github.com>
2023-04-30 12:27:24 +0300

    L20 : Authority and Actor instances on Clients
    SetReplicates(true) for SMagicProjectile and SPowerupActor.
    This spawns the powerups on both client and server (previously it was spawning only on the server since the spawning happens in GameMode which only exists on the server).
    Also spawning of SMagicProjectile from the server is now visible on the client (client spawning is still not working as it needs a server RPC).
    Updated PlayerControllerBP to only create HUD on each client via isLocalController check. This fixes a MessageLog error when exiting play.

--- Source/ActionRoguelike/Private/SMagicProjectile.cpp
+++ Source/ActionRoguelike/Private/SMagicProjectile.cpp
@@ -18,6 +18,8 @@ ASMagicProjectile::ASMagicProjectile()
 	SphereComp->OnComponentBeginOverlap.AddDynamic(this, &ASMagicProjectile::OnActorOverlap);
 
 	DamageAmount = 20.0f;
+
+	SetReplicates(true);
 }
 
 void ASMagicProjectile::OnActorOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
